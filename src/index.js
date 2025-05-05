9f0ba59e7b86035e2d91b2cd0d570310a58d7e06
Mohd Ayaan <mohdayaan8071@gmail.com>
2025-05-05 19:48:05 +0530

    added readme

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const connect = require('./config/database');
 const app = express();
-const TweetRepository = require('./repository/tweet-repository');
-const Comment = require('./models/comments');
 
 const PORT = 3000;
 
@@ -10,7 +8,4 @@ app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
   await connect();
   console.log('Connected to MongoDB');
-  const tweetRepository = new TweetRepository();
-  const tweet = await tweetRepository.getAll(0, 4);
-  console.log(tweet[0].contentWithEmail);
 });
